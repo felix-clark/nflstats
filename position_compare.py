df23f5525d0b506d6c260e68b97ff5df66c3a023
Felix Clark <mfclark3690@gmail.com>
2017-08-17 23:15:37 -0400

    using seaborn and pandas to graph positional comparison

--- position_compare.py
+++ position_compare.py
@@ -1,43 +1,27 @@
 #!/usr/bin/env python
 
 import nflgame
-
 import numpy as np
-from numpy import sqrt
-import matplotlib.pyplot as plt
+# from numpy import sqrt
+# import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 import dist_fit
 from ruleset import bro_league, phys_league
 
-def getPreseasonRanksPosYear( position, year ):
-    """
-    position: 'qb', 'rb', 'wr', 'te', 'k'
-    """
-    pos_dict = {}
+def getPreseasonRank( position, year, pname ):
     filename = 'preseason_rankings/top_' + position.lower() + 's_pre' + str(year) + '.txt'
     f = open(filename, 'r')
+    rank = None
     for l in f:
         ldata = l.split('. ')
-        rank = int( ldata[0] )
-        name = ldata[1].rstrip('\n')
-        pos_dict[rank] = name
-            
-    return pos_dict
-        
-        # players = [p for p in nflgame.find( name ) if p.position == position]
-        # if not players:
-        #     print 'Could not find player ', name
-        #     exit(1) # probably an overreaction
-        # if len( players ) > 1:
-        #     print 'Found multiple ', name, 's!'
-        #     print players
-        #     exit(1)
-        # player = players[0]
-        # pid = player.playerid
-        # if name != player.name:
-        #     print 'names don\'t match'
-        #     print name,player.name
-
+        if pname.lower() == '. '.join(ldata[1:]).rstrip('\n').lower():
+            rank = int( ldata[0] )
+            break
+    f.close()
+    return rank
+    
 # doesn't cover every rule, just the main ones.
 # some stats are not as easily extracted
 def getBasePoints( rs, plyr ):
@@ -70,129 +54,81 @@ def getBasePoints( rs, plyr ):
     return fpts
 
 
-qb_fpts = []
-rb_fpts = []
-wr_fpts = []
-te_fpts = []
-k_fpts = []
+# this list will be used as a constructor to the DataFrame
+# perhaps there is a slicker way to construct a DataFrame?
+datalist = []
 
+# at the moment we only have preseason ranking data for 2015 and 2016
+years = range(2015,2017)
 
-# for year in range(2009, 2017):
-for year in range(2016, 2017):
-    # year = 2009 # options range from 2009 to present
+for year in years:
     print 'processing {} season'.format( year )
 
-    qbs_pre_ranked = getPreseasonRanksPosYear( 'QB', year )
-    print qbs_pre_ranked
-    
     games = nflgame.games_gen( year )
     # get game-level stats
     all_players = nflgame.combine_game_stats( games )
     # top_qbs = all_players.passing().filter( playerid=lambda x: x in qb_ids ).sort('passing_yds').limit( 32 )
     # for some reason "position" does allow any through and we must use "guess_position"
-    top_qbs = all_players.passing().filter( guess_position='QB' ).sort('passing_yds').limit( 32 )
-    top_rbs = all_players.rushing().filter( guess_position='RB' ).sort('rushing_yds').limit( 64 )
-    top_wrs = all_players.receiving().filter( guess_position='WR' ).sort('receiving_yds').limit( 80 )
-    top_tes = all_players.receiving().filter( guess_position='TE' ).sort('receiving_yds').limit( 32 )
-    top_ks = all_players.kicking().filter( guess_position='K' ).sort('kicking_fgyds').limit( 32 )
+    top_players = {}
+    top_players['QB'] = all_players.passing().filter( guess_position='QB' ).sort('passing_yds').limit( 32 )
+    top_players['RB'] = all_players.rushing().filter( guess_position='RB' ).sort('rushing_yds').limit( 64 )
+    top_players['WR'] = all_players.receiving().filter( guess_position='WR' ).sort('receiving_yds').limit( 64 )
+    top_players['TE'] = all_players.receiving().filter( guess_position='TE' ).sort('receiving_yds').limit( 24 )
+    # checked that kickers have a very flat response vs. preseason rank.
+    # check again once we add bonuses?
+    # top_players['K'] = all_players.kicking().filter( guess_position='K' ).sort('kicking_fgyds').limit( 24 )
 
-    # print [rb.name for rb in top_rbs]  # uses up list; would need copy to do additional operations
-    
-    # top_player_names = [ p.player.full_name for p in all_players.passing().sort('passing_yds').limit( n_top_players ) ]
-    # top_playerids = [ p.playerid for p in all_players.passing().sort('passing_yds').limit( n_top_players ) ]
-
-    qb_rank_dict = {}
-    
-    for qbstat in top_qbs:
+    for (pos,players) in top_players.items():
+        for pstat in players:
         # print qbstat.rushing_lng, qbstat.rushing_lngtd # longest rush, longest TD rush
         # print qbstat.stats # dict of the stats
         # print qbstat.formatted_stats() # relatively nicely printed out version of the stats dict
-        base_pts = getBasePoints( bro_league, qbstat )
-        qb_fpts.append( base_pts )
-        qbrank = -1
-        for (rank,name) in qbs_pre_ranked.items():
-            if name == qbstat.player.full_name.strip():
-                qbrank = rank
-                break
-        if qbrank < 0:
-            print 'count not find ', qbstat.player.full_name, ' in top QBs.'
-        else:
-            qb_rank_dict[qbrank] = base_pts
-    for rbstat in top_rbs:
-        base_pts = getBasePoints( bro_league, rbstat )
-        rb_fpts.append( base_pts )
-    for wrstat in top_wrs:
-        base_pts = getBasePoints( bro_league, wrstat )
-        wr_fpts.append( base_pts )
-    for testat in top_tes:
-        base_pts = getBasePoints( bro_league, testat )
-        te_fpts.append( base_pts )
-    for kstat in top_ks:
-        # print dir(kstat)
-        base_pts = getBasePoints( bro_league, kstat )
-        k_fpts.append( base_pts )
-        
-        
-    # for week in range(1,18):
-    #     # print 'looking at week ', week
-    #     weekly_games = nflgame.games_gen( year, week )
-    #     weekly_player_stats = nflgame.combine_game_stats( weekly_games )
-    #     for pstat in weekly_player_stats.passing().filter( playerid=lambda x: x in top_playerids ):
-    #         # msg = '{} ({}): {} carries for {} yards and {} TDs'.format( pstat, pstat.player.full_name,
-    #         #                                                             pstat.rushing_att, pstat.rushing_yds,
-    #         #                                                             pstat.rushing_tds )
-    #         # print msg
-    #         # print dir(pstat) # to check methods
-    #         rshyd = pstat.rushing_yds
-    #         rshtd = pstat.rushing_tds
+            pfullname = pstat.player.full_name.strip()
+            base_pts = getBasePoints( bro_league, pstat )
+            # make a list of dicts to create a pandas data frame
+            ps_rank = getPreseasonRank( pos, year, pfullname )
+            datalist.append({'position':pos,
+                             'year':year,
+                             'name':pfullname,
+                             'preseason_rank':ps_rank,
+                             'fantasy_points':base_pts
+                             # 'playerid':pstat.playerid
+            })
 
-print 'season fantasy points:'
-print 'QB:'
-print qb_fpts
-print 'RB:'
-print rb_fpts
-print 'WR:'
-print wr_fpts
-print 'TE:'
-print te_fpts
-print 'K:'
-print k_fpts
-
-# in a single game
-# dist_fit.plot_counts( fantasy_points, label='points', fits=['neg_binomial'] )
-# dist_fit.plot_counts_poly( fantasy_points, bounds=(-50,400), label='total yards' )
-
-# there are games with negative yards so counts are not appropriate
-# print 'rushing yards:'
-# dist_fit.plot_counts( rush_yds, label='rushing yards' )
+            # for week in range(1,18):
+            #     # print 'looking at week ', week
+            #     weekly_games = nflgame.games_gen( year, week )
+            #     weekly_player_stats = nflgame.combine_game_stats( weekly_games )
+            #     for pstat in weekly_player_stats.passing().filter( playerid=lambda x: x in top_playerids ):
+            #         # msg = '{} ({}): {} carries for {} yards and {} TDs'.format( pstat, pstat.player.full_name,
+            #         #                                                             pstat.rushing_att, pstat.rushing_yds,
+            #         #                                                             pstat.rushing_tds )
+            #         # print msg
+            #         # print dir(pstat) # to check methods
+            #         rshyd = pstat.rushing_yds
+            #         rshtd = pstat.rushing_tds
 
 
-# for qbn in qb_names:
-#     qbs += nflgame.find( qbn )
-# # player = None
-# # players = nflgame.find( player_name )
-# # if not players:
-# #     print 'Could not find player ', player_name
-# #     exit(1)
-# # if len( players ) > 1:
-# #     print 'Found multiple ', player_name, 's!'
-# #     exit(1)
-# print [qb.name for qb in qbs]
-# print [qb.position for qb in qbs]
-# qb_ids = [qb.playerid for qb in qbs]
+# can and should factor the reading of data from nflgame
+# and the plotting of it.
+# use DataFrame.read_csv(<filename>) (and to_csv(<filename>) ?)
+            
+df = pd.DataFrame(datalist)    
+# print df
 
+# everything looks better with a default set() call
+sns.set()
 
-# def get_rush_att( player_name = 'todd gurley', year = 2013 ):
-#     '''
-#     player: 
-#     year: season ( >= 2009 )
-#     '''
+g = sns.lmplot(x='preseason_rank',y='fantasy_points',hue='year',
+               # can also use "items" to select specific branches in the filter
+               data=df[df.position=='QB'][df.preseason_rank<=28]
+               )
+g.set_axis_labels('ESPN staff preseason ranking', 'yearly fantasy points (w/out bonus)')
+g.savefig('output.png')
 
-# player = None
-# players = nflgame.find( player_name )
-# if not players:
-#     print 'Could not find player ', player_name
-#     exit(1)
-# if len( players ) > 1:
-#     print 'Found multiple ', player_name, 's!'
-#     exit(1)
+g = sns.lmplot(x='preseason_rank',y='fantasy_points',hue='position',
+               data=df[df.preseason_rank<=16]
+               )
+g.set_axis_labels('ESPN staff preseason ranking', 'yearly fantasy points (w/out bonus)')
+g.savefig('compare_pos.png')
+
