6d95c520a74690d91a4abb5ff0930b813de56355
Felix Clark <mfclark3690@gmail.com>
2020-06-21 08:58:58 -0400

    adding draft app to scripts

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(name='nflstats',
-      version='0.4.0',
-      packages=find_packages())
+setup(
+    name="nflstats", version="0.4.0", packages=find_packages(), scripts=["draft_app.py"]
+)
