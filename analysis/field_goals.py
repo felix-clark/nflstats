f5c379097f6fdf7859daa211816cb1f4e98a7dc2
Felix Clark <mfclark3690@gmail.com>
2020-07-03 16:59:38 -0400

    splines for distance and controling for global probability

--- analysis/field_goals.py
+++ analysis/field_goals.py
@@ -1,37 +1,111 @@
 #!/usr/bin/env python3
 
-from typing import Any, Dict, Iterable, List, Optional, Tuple
+from itertools import product
+from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.special as sf
 import statsmodels.api as sm
 from db import plays
 from nptyping import NDArray
-from sklearn.linear_model import LogisticRegression
+from statsmodels.gam.api import BSplines
 from statsmodels.genmod.generalized_linear_model import GLMResults
-
-# from utility import split_test_train
+from utility import split_test_train
 
 # TODO:
 # Control for roof type (outdoors, dome, etc.), possibly weather. Some data is missing.
 
 
 def get_loss(
-    model: LogisticRegression, x_data_test: pd.DataFrame, y_data_test: pd.Series
+    model: GLMResults, x_data_test: pd.DataFrame, y_data_test: pd.Series
 ) -> float:
     """
     Return logistic loss from model
     """
-    assert (
-        model.classes_ == np.array([0.0, 1.0])
-    ).all(), "Y classes have incorrect order"
-    test_log_prob: NDArray[float] = model.predict_log_proba(x_data_test)
-    loss_terms: NDArray[float] = -y_data_test * test_log_prob.T[1] - (
+    # These are the predicted probabilities of making a FG
+    predict = model.predict(x_data_test)
+    loss_terms: NDArray[float] = -y_data_test * np.log(predict) - (
         1.0 - y_data_test
-    ) * test_log_prob.T[0]
+    ) * np.log(1.0 - predict)
     loss: float = np.sum(loss_terms) / len(y_data_test)
+    # assert (
+    #     model.classes_ == np.array([0.0, 1.0])
+    # ).all(), "Y classes have incorrect order"
+    # test_log_prob: NDArray[float] = model.predict_log_proba(x_data_test)
+    # loss_terms: NDArray[float] = -y_data_test * test_log_prob.T[1] - (
+    #     1.0 - y_data_test
+    # ) * test_log_prob.T[0]
     return loss
+
+
+def basic_gam(data: pd.DataFrame, save_pred: bool = False) -> GLMResults:
+    """
+    Regression of success probability as a function of kick distance
+    """
+    # TODO: treat blocked FGs separately
+    data["fg_make"] = data["field_goal_result"].map(
+        # {"made": True, "missed": False, "blocked": False}
+        {"made": 1, "missed": 0, "blocked": 0}
+    )
+    train_params = ["kick_distance"]
+    # data_test, data_train = split_test_train(data)
+    # x_data_train = data_train[train_params]
+    # y_data_train = data_train["fg_make"]
+    # x_data_test = data_test[train_params]
+    # y_data_test = data_test["fg_make"]
+    x_data_train = data[train_params]
+    y_data_train = data["fg_make"]
+
+    # These values should be tested by cross-validation
+    # degree 3 has slightly better test loss than 2 and isn't noticeably worse than 4
+    degree = 3
+    # df = 4 has the least test loss but is the minimum required for degree 3
+    df = 4
+    # alpha > 0 results in increased test loss. select_penweight() can be used to choose
+    # in general.
+    alpha = 0.0
+    bs = BSplines(x_data_train, df=[df], degree=[degree])
+
+    model = sm.GLMGam(
+        y_data_train,
+        sm.add_constant(x_data_train[[]]),
+        smoother=bs,
+        alpha=alpha,
+        family=sm.families.Binomial(),
+    )
+
+    fit = model.fit()
+
+    # test_loss = get_loss(
+    #     fit,
+    #     sm.add_constant(bs.transform(x_data_test.to_numpy())),
+    #     y_data_test
+    # )
+    df_resid = fit.df_resid
+    llf = fit.llf / df_resid
+    deviance = fit.deviance / df_resid
+    chi_sq_df = fit.pearson_chi2 / df_resid
+    print(f"ll / ndf = {llf}")
+    print(f"deviance = {deviance}")
+    print(f"chi sq / ndf = {chi_sq_df}")
+    print(f"AIC = {fit.aic}")
+
+    if save_pred:
+        data["fg_make_prob"] = fit.predict(
+            sm.add_constant(bs.transform(data[train_params].to_numpy()))
+        )
+
+    # defaults to 95% confidence interval (0.05 argument is alpha)
+    # print(fit.conf_int(0.1))
+    # standard error approximation (95% ~ 2*sigma, double-sided)
+    # print(0.25 * (fit.conf_int()[1] - fit.conf_int()[0]))
+    # print(dir(sm_model_fit))
+
+    print(f"params = {fit.params}")
+
+    return fit, bs
 
 
 def basic_regression(data: pd.DataFrame) -> GLMResults:
@@ -67,6 +141,7 @@ def basic_regression(data: pd.DataFrame) -> GLMResults:
     print(f"ll / ndf = {sm_loss}")
     print(f"deviance = {sm_deviance}")
     print(f"chi sq / ndf = {sm_chi_sq_df}")
+    print(f"AIC = {sm_fit.aic}")
 
     # print(sm_model_fit.summary2())
     print(sm_fit.params)
@@ -84,7 +159,8 @@ def basic_regression(data: pd.DataFrame) -> GLMResults:
 
 def player_regression_range(data: pd.DataFrame) -> pd.DataFrame:
     """
-    Regression of success probability treating each player's whole career separately
+    Regression of success probability treating each player's whole career separately.
+    Offsets represent the general probabilities, so the parameters are per-player offsets.
     """
     data["fg_make"] = data["field_goal_result"].map(
         {"made": 1.0, "missed": 0.0, "blocked": 0.0}
@@ -94,13 +170,16 @@ def player_regression_range(data: pd.DataFrame) -> pd.DataFrame:
     kicker_results: List[Dict[str, Any]] = []
     for kicker_id in kicker_ids:
         kicker_attempts = data[data["kicker_player_id"] == kicker_id]
+        offsets = sf.logit(kicker_attempts["fg_make_prob"])
         kicker_model = sm.GLM(
             kicker_attempts["fg_make"],
             sm.add_constant(kicker_attempts["kick_distance"]),
             family=sm.families.Binomial(),
+            offset=offsets,
         )
         kicker_fit = kicker_model.fit()
         params = kicker_fit.params
+        print(params)
         # 95% CI by default: use 1 sigma
         conf_int = kicker_fit.conf_int(0.3173)
         kicker_results.append(
@@ -121,6 +200,52 @@ def player_regression_range(data: pd.DataFrame) -> pd.DataFrame:
     return kicker_data
 
 
+def player_regression_range_season(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Regression of success probability year-by-year allowing for individual distance terms.
+    Offsets represent the general probabilities, so the parameters are per-player offsets.
+    """
+    data["fg_make"] = data["field_goal_result"].map(
+        {"made": 1.0, "missed": 0.0, "blocked": 0.0}
+    )
+
+    kicker_ids: List[str] = data["kicker_player_id"].unique().tolist()
+    kicker_results: List[Dict[str, Any]] = []
+    for kicker_id in kicker_ids:
+        kicker_attempts = data[data["kicker_player_id"] == kicker_id]
+        years = kicker_attempts["season"].unique()
+        for year in years:
+            year_attempts = kicker_attempts[kicker_attempts['season'] == year]
+            offsets = sf.logit(year_attempts["fg_make_prob"])
+            kicker_model = sm.GLM(
+                year_attempts["fg_make"],
+                sm.add_constant(year_attempts["kick_distance"]),
+                family=sm.families.Binomial(),
+                offset=offsets,
+            )
+            kicker_fit = kicker_model.fit()
+            params = kicker_fit.params
+            # 95% CI by default: use 1 sigma
+            conf_int = kicker_fit.conf_int(0.3173)
+            kicker_results.append(
+                {
+                    "id": kicker_id,
+                    "name": get_player_name(year_attempts, kicker_id),
+                    "year": year,
+                    "const": kicker_fit.params["const"],
+                    "kick_distance": params["kick_distance"],
+                    "const_err_low": params["const"] - conf_int.loc["const", 0],
+                    "const_err_up": conf_int.loc["const", 1] - params["const"],
+                    "kick_distance_err_low": params["kick_distance"]
+                    - conf_int.loc["kick_distance", 0],
+                    "kick_distance_err_up": conf_int.loc["kick_distance", 1]
+                    - params["kick_distance"],
+                }
+            )
+    kicker_data = pd.DataFrame(kicker_results)
+    return kicker_data
+
+
 def player_regression(data: pd.DataFrame) -> Tuple[GLMResults, pd.DataFrame]:
     """
     Regression of success probability treating each player's whole career separately,
@@ -135,7 +260,9 @@ def player_regression(data: pd.DataFrame) -> Tuple[GLMResults, pd.DataFrame]:
     data_encoded = pd.concat([data, kicker_one_hot], axis=1)
     # TODO: use a GAM and spline the kick_distance variable. This will re-necessitate
     # training data to find the best alpha penalties.
-    train_params = ["kick_distance"] + kicker_one_hot.keys().tolist()
+    # train_params = ["kick_distance"] + kicker_one_hot.keys().tolist()
+    # When using offsets, don't use kick distance
+    train_params = kicker_one_hot.keys().tolist()
     # data_test, data_train = split_test_train(data_encoded)
     # x_data_test = data_test[train_params]
     # y_data_test = data_test["fg_make"]
@@ -144,7 +271,10 @@ def player_regression(data: pd.DataFrame) -> Tuple[GLMResults, pd.DataFrame]:
     x_data_train = data_encoded[train_params]
     y_data_train = data_encoded["fg_make"]
     # don't add_constant() for this x-data
-    model = sm.GLM(y_data_train, x_data_train, family=sm.families.Binomial())
+    offsets = sf.logit(data_encoded["fg_make_prob"])
+    model = sm.GLM(
+        y_data_train, x_data_train, family=sm.families.Binomial(), offset=offsets
+    )
     sm_fit = model.fit()
     # print(sm_fit.summary2())
 
@@ -159,8 +289,10 @@ def player_regression(data: pd.DataFrame) -> Tuple[GLMResults, pd.DataFrame]:
     print(f"chi sq / ndf = {sm_chi_sq_df}")
     # print(sm_fit.params.drop("kick_distance"))
 
-    kicker_params: List[float] = sm_fit.params.drop("kick_distance")
-    kicker_ids: List[str] = train_params[1:]
+    # kicker_params: List[float] = sm_fit.params.drop("kick_distance")
+    kicker_params: List[float] = sm_fit.params
+    # kicker_ids: List[str] = train_params[1:]
+    kicker_ids: List[str] = train_params
     kicker_data: pd.DataFrame = pd.DataFrame(
         data={"score": kicker_params, "id": kicker_ids}
     )
@@ -182,9 +314,8 @@ def player_regression_season(data: pd.DataFrame) -> Tuple[GLMResults, pd.DataFra
     kicker_season_labels = data["kicker_player_id"] + "_" + data["season"].apply(str)
     kicker_one_hot = pd.get_dummies(kicker_season_labels)
     data_encoded = pd.concat([data, kicker_one_hot], axis=1)
-    # TODO: use a GAM and spline the kick_distance variable. This will re-necessitate
-    # training data to find the best alpha penalties.
-    train_params = ["kick_distance"] + kicker_one_hot.keys().tolist()
+    # train_params = ["kick_distance"] + kicker_one_hot.keys().tolist()
+    train_params = kicker_one_hot.keys().tolist()
     # data_test, data_train = split_test_train(data_encoded)
     # x_data_test = data_test[train_params]
     # y_data_test = data_test["fg_make"]
@@ -193,7 +324,11 @@ def player_regression_season(data: pd.DataFrame) -> Tuple[GLMResults, pd.DataFra
     x_data_train = data_encoded[train_params]
     y_data_train = data_encoded["fg_make"]
     # don't add_constant() for this x-data
-    model = sm.GLM(y_data_train, x_data_train, family=sm.families.Binomial())
+
+    offsets = sf.logit(data_encoded["fg_make_prob"])
+    model = sm.GLM(
+        y_data_train, x_data_train, family=sm.families.Binomial(), offset=offsets
+    )
     fit = model.fit()
     # print(sm_fit.summary2())
 
@@ -207,7 +342,8 @@ def player_regression_season(data: pd.DataFrame) -> Tuple[GLMResults, pd.DataFra
     print(f"chi sq / ndf = {sm_chi_sq_df}")
     # print(sm_fit.params.drop("kick_distance"))
 
-    kicker_year_params: pd.Series = fit.params.drop("kick_distance")
+    kicker_year_params: pd.Series = fit.params
+    # kicker_year_params: pd.Series = fit.params.drop("kick_distance")
     season_data: List[Dict[str, Any]] = []
     for id_year, accuracy in kicker_year_params.items():
         kicker_id, year = id_year.split("_")
@@ -266,11 +402,16 @@ def _get_data(columns: Optional[Iterable[str]] = None) -> pd.DataFrame:
     return play_data
 
 
-def plot_fg_prob(fg_attempts: pd.DataFrame, model: GLMResults, label: str = "") -> None:
+def plot_fg_prob(
+    fg_attempts: pd.DataFrame,
+    model: GLMResults,
+    transform: Optional[Callable[[NDArray[float]], NDArray[float]]] = None,
+    **kwargs: Any,
+) -> None:
     """
     Make a plot of the fg prob
     """
-    label = label or "fg"
+    label = kwargs.get("label", "fg")
     yard_bins = np.arange(15, 65, 1)
     # fg_attempts['dist_bin'] = pd.cut(x=fg_attempts['kick_distance'], bins=yard_bins)
     data_bins = pd.cut(x=fg_attempts["kick_distance"], bins=yard_bins)
@@ -286,8 +427,11 @@ def plot_fg_prob(fg_attempts: pd.DataFrame, model: GLMResults, label: str = "") 
         yavg = yvals.mean()
         y.append(yavg)
         yerr.append(np.sqrt(yavg * (1 - yavg) / (len(bin_data) - 1)))
-    xfit = np.arange(15, 70, 0.1)
-    yfit = model.predict(sm.add_constant(xfit.reshape(-1, 1)))
+    xfit = np.arange(min(x), max(x) - 0.1, 0.1)
+    xshape = xfit.reshape(-1, 1)
+    if transform is not None:
+        xshape = transform(xshape)
+    yfit = model.predict(sm.add_constant(xshape))
     # yfit = model.predict_proba(xfit.reshape(-1, 1)).T[1]
     # xfit2 = np.stack([xfit, xfit**2], axis=1)
     # yfit = dist_reg.predict_proba(xfit2).T[1]
@@ -367,7 +511,20 @@ def main():
     ]
 
     fg_attempts = _get_data(columns=keep_columns)
-    # print(fg_attempts)
+
+    inc_model = basic_regression(fg_attempts)
+    # This model describes the probability as a function of distance very accurately, so
+    # save its predictions to use as a baseline in future models.
+    gam_model, bspline = basic_gam(fg_attempts, save_pred=True)
+
+    # Plot these before removing less experienced kickers
+    fig = plt.figure()
+    plot_fg_prob(fg_attempts, inc_model)
+    fig.savefig(f"fg.png")
+
+    fig = plt.figure()
+    plot_fg_prob(fg_attempts, gam_model, transform=bspline.transform)
+    fig.savefig(f"fg_gam.png")
 
     # Remove kickers with few attempts
     attempt_ids = fg_attempts["kicker_player_id"]
@@ -380,12 +537,6 @@ def main():
         fg_attempts["kicker_player_id"].isin(good_id_counts.index)
     ]
     # print(len(fg_attempts))
-
-    # test_att = fg_attempts.iloc[0]
-    # for key, val in test_att.items():
-    #     print(key, val)
-
-    inc_model = basic_regression(fg_attempts)
 
     player_model, kicker_eval = player_regression(fg_attempts)
     kicker_eval["name"] = kicker_eval["id"].apply(
@@ -407,17 +558,15 @@ def main():
     )
     print(kicker_season_data)
 
+    # some seasons have perfect separation so this doesn't work out-of-the-box
+    # kicker_season_range_data = player_regression_range_season(fg_attempts)
+    # print(kicker_season_range_data)
+
     # print(fg_attempts["kicker_player_name"].unique())
     # jt_id: str = "32013030-2d30-3032-3935-39371b9a6ac1"
     # jt_data = fg_attempts[fg_attempts["kicker_player_id"] == jt_id].copy()
     # jt_model = basic_regression(jt_data)
     # print(jt_model.intercept_, jt_model.coef_)
-
-    # Plot
-    fig = plt.figure()
-    plot_fg_prob(fg_attempts, inc_model)
-    # plot_fg_prob(jt_data, jt_model, label="J.Tucker")
-    fig.savefig(f"fg.png")
 
     fig = plt.figure()
     plot_kicker_career(kicker_ind_range)
